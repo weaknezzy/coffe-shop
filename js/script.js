fad85617bd15b8e6dde91336aa1e3d665fc4432e
Yoga Pradana <132411206+weaknezzy@users.noreply.github.com>
2025-11-28 10:31:23 +0700

    membuat tombol search responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toogle class active
+// Toogle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika hamburger menu di click
@@ -6,11 +6,26 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+//toggle class aktif untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
