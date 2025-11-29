c7eac2f8d2f3816d1b3548cd5456aaef1d0af178
Yoga Pradana <132411206+weaknezzy@users.noreply.github.com>
2025-11-29 11:55:09 +0700

    membuat detail produk menggunakan modal

--- js/script.js
+++ js/script.js
@@ -41,3 +41,27 @@ document.addEventListener("click", function (e) {
     navbarShop.classList.remove("active");
   }
 });
+
+// modal box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik diluar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
